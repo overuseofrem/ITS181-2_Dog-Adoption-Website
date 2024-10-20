83ff4411d1475798d95810f90609f8c3c53b383f
overuseofrem <francinetaing13@gmail.com>
2024-10-20 16:58:03 +0700

    Added more components and organized component files
    I've split the different components into [components], [layout], and [pages]. So, single UI stuff like the dog-cards for the dog grid go into the [components], [layout] has stuff like sidenav and footer, and [pages] are the general structure of our webpage types like grid and main.

---
+++ dog-adoption-site/src/app/components/dog-card/dog-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dog-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './dog-card.component.html',
+  styleUrl: './dog-card.component.css'
+})
+export class DogCardComponent {
+
+}
---
+++ dog-adoption-site/src/app/pages/adoptable-dogs/adoptable-dogs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adoptable-dogs',
+  standalone: true,
+  imports: [],
+  templateUrl: './adoptable-dogs.component.html',
+  styleUrl: './adoptable-dogs.component.css'
+})
+export class AdoptableDogsComponent {
+
+}
