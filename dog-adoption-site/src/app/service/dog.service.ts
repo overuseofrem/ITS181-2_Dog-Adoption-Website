413e22aba03fe6f674bf4469e5da011e9be67e6d
overuseofrem <francinetaing13@gmail.com>
2024-10-22 02:02:02 +0700

    updated Dog model and Dog service

--- dog-adoption-site/src/app/service/dog.service.ts
+++ dog-adoption-site/src/app/service/dog.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Dog } from '../model/dog';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DogService {
 
-  constructor() { }
+  private apiUrl = '###' // update this with SpringBoot API URL
+
+  constructor(private http: HttpClient) { }
+
+  getDogs(): Observable<Dog[]> {
+
+    return this.http.get<Dog[]>(this.apiUrl);
+
+  }
+
 }
