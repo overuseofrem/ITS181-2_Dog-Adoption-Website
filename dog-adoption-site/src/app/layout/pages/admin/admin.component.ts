9fdacf26049d5b5cabda9c0a6c18d8d95f11065e
Ruby <99315167+rjventu@users.noreply.github.com>
2024-10-30 15:50:41 +0800

    added account card to admin tables
    + improved admin page
    + added account card to admin-dogs and admin-adoptions

--- dog-adoption-site/src/app/layout/pages/admin/admin.component.ts
+++ dog-adoption-site/src/app/layout/pages/admin/admin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
