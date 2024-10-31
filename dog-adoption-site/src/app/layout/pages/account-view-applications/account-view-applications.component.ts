8fa6700135c44a4b54fd78e83225775f9fd57c50
Ruby <99315167+rjventu@users.noreply.github.com>
2024-10-31 16:12:32 +0800

    fixed some pages
    modified the appearances of some pages:
    + account view applications (improved dog card)
    + account (improved dog card and account card)
    + admin-dog-form (included new dog attributes)

--- dog-adoption-site/src/app/layout/pages/account-view-applications/account-view-applications.component.ts
+++ dog-adoption-site/src/app/layout/pages/account-view-applications/account-view-applications.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-account-view-applications',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './account-view-applications.component.html',
   styleUrl: './account-view-applications.component.css'
 })
