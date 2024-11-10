e9054f90ea8bb01f6736068d9235712e5f4a6964
Ruby <99315167+rjventu@users.noreply.github.com>
2024-11-10 13:10:18 +0800

    backend integration
    - added backend files
    - renamed dog-adoption-site to frontend
    - added sql file

---
+++ backend/src/main/java/com/df/doggoforever/model/Dog.java
@@ -0,0 +1,33 @@
+package com.df.doggoforever.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="dog")
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+    private String gender;
+    private boolean vacc;
+    private boolean ster;
+    private String description;
+    private String img;
+
+    @OneToMany(mappedBy = "dog", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Adoption> adoptions = new HashSet<>();
+
+}
+
+
---
+++ backend/src/main/java/com/df/doggoforever/service/AuthService.java
@@ -0,0 +1,26 @@
+package com.df.doggoforever.service;
+
+import com.df.doggoforever.model.User;
+import com.df.doggoforever.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public User matchesUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    public boolean usernameIsTaken (String username) {
+        User existingUser = userRepository.findByUsername(username).orElse(null);
+        return existingUser != null;
+    }
+
+}
